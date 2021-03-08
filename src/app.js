e2f597cda48f3b8e3e715f9a2b4d4bd90973c136
hiennguyen <hien.nguyen2@sotatek.com>
2021-03-08 17:36:37 +0700

    commit css

--- src/app.js
+++ src/app.js
@@ -7,18 +7,23 @@ const port = 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(morgan('combined'));
+// app.use(morgan('combined'));
 
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
 
 app.get('/', function (req, res) {
-    res.render('home');
+  return  res.render('home');
 })
 
-app.get('/new', function (req, res) {
-  res.render('new');
+app.get('/news', function (req, res) {
+  return res.render('news');
+})
+
+app.get('/search', function (req, res) {
+  console.log("req : ", req.query);
+  return res.render('search');
 })
 
 app.listen(port, () => {
